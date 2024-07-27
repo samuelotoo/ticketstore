46466a752b8bda2dadd6e46ad4b394f222c8b97b
Samuel Otoo <primecyfer@gmail.com>
2024-07-27 04:29:20 +0000

    auth login page

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,9 +1,20 @@
+"use client";
+
 import React from "react";
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+
+    console.log("Login deetails", email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Log in</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
+import AuthForm from "../AuthForm";
 
 export default function Signup() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+
+    console.log("signup details", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
