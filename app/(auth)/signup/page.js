8152fd16ec67f32721a0cfcf5e40932dbbd497e6
Samuel Otoo <primecyfer@gmail.com>
2024-08-07 15:17:41 +0000

    user authentication

--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,19 +1,38 @@
 "use client";
+import { createClient } from "@supabase/supabase-js";
 
-import React from "react";
+import React, { useState } from "react";
 import AuthForm from "../AuthForm";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
 
-    console.log("signup details", email, password);
+    const supabase = createClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/calback`,
+      },
+    });
+    if (error) {
+      setError(error.message);
+    }
+    if (!error) {
+      router.push("/verify");
+    }
   };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
